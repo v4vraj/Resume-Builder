1c6735704a1953316c86da95a5763048e48f9661
vraj1763@gmail.com <vraj1763@gmail.com>
2024-04-13 22:28:30 +0530

    target added

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        // target: "http://localhost:10000",
+        target: "https://resume-builder-api-3i9n.onrender.com",
         changeOrigin: true,
       },
     },
